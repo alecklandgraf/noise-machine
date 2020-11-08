b843c9c81316db3e34bf6698b0789bb4320f906c
Aleck Landgraf <aleck.landgraf@airbnb.com>
2020-11-07 17:40:53 -0800

    offline first app

---
+++ sw.js
@@ -0,0 +1,32 @@
+self.addEventListener("install", function (event) {
+  self.skipWaiting();
+});
+
+const CACHE_NAME = "v1";
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        console.log(`serving ${event.request.url} from cache`);
+        return response;
+      }
+
+      return fetch(event.request).then(function (response) {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        const responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
